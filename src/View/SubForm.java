aacaa3c9a3a5ca1268d63b84be83521237a4bbc9
NDThong <65402387+thongnd0200@users.noreply.github.com>
2020-06-29 09:55:50 +0700

    Update SubForm.java
    Điều chỉnh lại format code theo nhận xét của mentors

--- src/View/SubForm.java
+++ src/View/SubForm.java
@@ -14,6 +14,7 @@ import java.sql.SQLException;
 import java.util.Vector;
 
 public class SubForm extends JFrame{
+	
 	DataManager manager = new DataManager();
 	JPanel contentPane;
 	JScrollPane scrollPane;
@@ -22,57 +23,65 @@ public class SubForm extends JFrame{
 	GUI gui;
 	Vector<Vector<String>> data = new Vector<Vector<String>>();
 	Vector<String> header = new Vector<String>();
+	
 	public SubForm(GUI gui) throws ClassNotFoundException, SQLException {
+		
 		this.gui = gui;
 		this.setSize(900, 500);
 		this.setTitle("I/O History");
 		contentPane = new JPanel();
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
 		lable = new JLabel("I/O History");
 		lable.setBounds(380, 5, 300, 40);
 		lable.setFont(new Font("arial",Font.BOLD, 30));
 		contentPane.add(lable);
+		
 		dataTable = new JTable();
 		dataTable.setRowSelectionAllowed(true);
 		dataTable.setDefaultEditor(Object.class, null);
-	    dataTable.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+	        dataTable.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
 	    
-//	    dataTable.getColumnModel().getColumn(0).setPreferredWidth(10);
-	    scrollPane = new JScrollPane(dataTable);
-	    scrollPane.setBounds(50, 50, 800, 350);
+	    	scrollPane = new JScrollPane(dataTable);
+	    	scrollPane.setBounds(50, 50, 800, 350);
 	    
-	    data = manager.getHistory(); 
+	    	data = manager.getHistory(); 
 	    
-	    header.add("STT");
-	    header.add("Input");
-	    header.add("Output");
-	    header.add("DataAndTime");
+	    	header.add("STT");
+	   	header.add("Input");
+	   	header.add("Output");
+	    	header.add("DataAndTime");
 		((DefaultTableModel) (dataTable.getModel())).setDataVector(data, header);
 		TableColumnModel columnModel = dataTable.getColumnModel();
-	    columnModel.getColumn(0).setPreferredWidth(50);
-	    columnModel.getColumn(1).setPreferredWidth(150);
-	    columnModel.getColumn(2).setPreferredWidth(400);
-	    columnModel.getColumn(3).setPreferredWidth(200);
-	    dataTable.setRowHeight(30);
-	    contentPane.add(scrollPane);
-	    dataTable.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-            	tblHistoryyMouseClicked(evt);
-            }
-        });
-	    this.setLocationRelativeTo(null);
-	    setVisible(true);
+		
+	    	columnModel.getColumn(0).setPreferredWidth(50);
+	    	columnModel.getColumn(1).setPreferredWidth(150);
+	    	columnModel.getColumn(2).setPreferredWidth(400);
+	    	columnModel.getColumn(3).setPreferredWidth(200);
+	    	dataTable.setRowHeight(30);
+	    	contentPane.add(scrollPane);
+		
+	    	dataTable.addMouseListener(new java.awt.event.MouseAdapter() {
+			
+            		public void mouseClicked(java.awt.event.MouseEvent evt) {
+            		tblHistoryyMouseClicked(evt);
+            	        }
+        	});
+	    	this.setLocationRelativeTo(null);
+	    	setVisible(true);
 	}
-	    public void tblHistoryyMouseClicked(MouseEvent evt) {
-			if (evt.getClickCount() == 2 && !evt.isConsumed()) {
-				evt.consume();
-				DefaultTableModel model = (DefaultTableModel)dataTable.getModel();
-				int selectedRow = dataTable.getSelectedRow();
-				gui.tfNhap.setText(model.getValueAt(selectedRow, 1).toString());
-				gui.tfResult.setText("");
-				setVisible(false);
-			}
+	
+	public void tblHistoryyMouseClicked(MouseEvent evt) {
+		
+		if (evt.getClickCount() == 2 && !evt.isConsumed()) {
+			evt.consume();
+			DefaultTableModel model = (DefaultTableModel)dataTable.getModel();
+			int selectedRow = dataTable.getSelectedRow();
+			gui.tfNhap.setText(model.getValueAt(selectedRow, 1).toString());
+			gui.tfResult.setText("");
+			setVisible(false);
+		}
 		
 	}
 }
