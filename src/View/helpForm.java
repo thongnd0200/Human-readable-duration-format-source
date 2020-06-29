cb5e455c97f345aa8e2d7d0e78160664e5d89de1
NDThong <65402387+thongnd0200@users.noreply.github.com>
2020-06-29 10:13:17 +0700

    Update helpForm.java
    Điều chỉnh lại format code theo nhận xét của mentors

--- src/View/helpForm.java
+++ src/View/helpForm.java
@@ -7,16 +7,20 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 public class helpForm extends JFrame {
+	
 	GUI gui;
 	JPanel contentPane;
 	JTextArea text;
+	
 	public helpForm(GUI gui) {
 		this.gui = gui;
 		this.setTitle("Help");
 		this.setSize(400, 300);
+		
 		contentPane = new JPanel();
 		setContentPane(contentPane);
-		contentPane.setLayout(null);		
+		contentPane.setLayout(null);	
+		
 		text = new JTextArea("Hướng dẫn sử dụng: \nInput là một số nguyên không âm với\nđơn vị là giây, Output sẽ là khoảng thời\ngian tương ứng.");
 		text.setBounds(10, 10, 360, 230);
 		text.setFont(new Font("Arial", Font.ROMAN_BASELINE, 20));
